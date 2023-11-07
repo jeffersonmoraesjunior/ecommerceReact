1b52acfd9e914b63800ee4fa1de7baf93deaafd7
renanpinela <renanpinela@gmail.com>
2023-11-06 23:14:12 -0300

    commit

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,18 +3,19 @@ import NavBar from '../../components/Navbar/Navbar';
 import Container from '../../components/Navbar/Container';
 import FooterContainer from '../../components/Footer/index';
 
+// import Carousel from '../../components/Carousel/carousel'
+
 export default function Home() {
 
     document.title = 'Home | Serracommerce'
     return (
         <>
             <NavBar />
+            {/* <Carousel/> */}
             <Container>
                 <Outlet />
             </Container>
             <FooterContainer />
         </>
     );
-
 }
-
